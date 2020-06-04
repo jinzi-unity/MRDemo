a7e3e694e6460ee1ddbd4f57657e210ec02f6c88
jinzi-unity <990500315@qq.com>
2020-06-04 16:22:57 +0800

    master分支提交一次更新

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,13 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int a = 1;
+            int b = 2;
+
+            int c = a + b;
+
+            Console.WriteLine(c);
         }
     }
 }
